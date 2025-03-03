8ce3e487f853d6c8da98767280c99eea0bf6f854
Barsol6 <bartosz.solyga@gmail.com>
2025-03-03 11:49:47 +0100

    refactor: changed to single page, added tailwind

--- mixi/Modules/UI/SignUpPopup.cs
+++ mixi/Modules/UI/SignUpPopup.cs
@@ -6,6 +6,9 @@ public class SignUpPopup
 {
  private bool _isVisible;
  public event Action? IsVisibleChange;
+ public event Action? IsLoggedChange;
+
+ private bool _isLogged;
  public bool IsVisible
  {
   get => _isVisible;
@@ -18,12 +21,21 @@ public class SignUpPopup
    }
   }
  }
+ 
+ public bool IsLogged
+ {
+  get => _isLogged;
+  set
+  {
+   if (_isLogged != value)
+   {
+    _isLogged = value;
+    IsLoggedChange?.Invoke();
+   }
+  }
+ }
  public string Username { get; set; } = String.Empty;
  public string Password { get; set; } = String.Empty;
  public string PasswordRepeat { get; set; } = String.Empty;
-
- public void dupa()
- {
-  Console.Out.Write(IsVisible);
- }
+ 
 }
