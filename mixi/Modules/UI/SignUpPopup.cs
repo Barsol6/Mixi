c7e6dae83447488fa158869772f8e0a105c297ee
Barsol6 <bartosz.solyga@gmail.com>
2025-03-13 08:44:13 +0100

    refactor: small changes in loginPage UI

--- mixi/Modules/UI/SignUpPopup.cs
+++ mixi/Modules/UI/SignUpPopup.cs
@@ -4,7 +4,7 @@ namespace mixi.Modules.UI;
 
 public class SignUpPopup
 {
- private bool _isVisible;
+ private bool _isVisible = false;
  public event Action? IsVisibleChange;
  public event Action? IsLoggedChange;
 
