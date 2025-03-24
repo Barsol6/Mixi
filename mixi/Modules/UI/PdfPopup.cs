2a6d4180a78331624153a5bd84361d7bad67ea06
Barsol6 <bartosz.solyga@gmail.com>
2025-03-24 13:06:13 +0100

    feat: added pdf viewer and sample pdf file

---
+++ mixi/Modules/UI/PdfPopup.cs
@@ -0,0 +1,21 @@
+﻿namespace mixi.Modules.UI;
+
+public class PdfPopup
+{
+    private string _pdfName = String.Empty;
+    
+    public event Action? PdfNameChanged;
+    
+    public string PdfName
+    {
+        get => _pdfName;
+        set
+        {
+            if (_pdfName != value)
+            {
+                _pdfName = value;
+                PdfNameChanged?.Invoke();
+            }
+        }
+    }
+}
