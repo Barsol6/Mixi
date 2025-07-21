ff90fc346e849ecf0a1f78313f1b4718b6da86c9
Barsol6 <bartosz.solyga@gmail.com>
2025-07-21 15:36:39 +0200

    refactor: added missing interface, removed unneccessary comments

---
+++ mixi/Modules/Database/Repositories/PdfRepositories/IPdfRepository.cs
@@ -0,0 +1,14 @@
+﻿using mixi.Modules.Pdf;
+
+namespace mixi.Modules.Database;
+
+public interface IPdfRepository
+{
+    Task<PdfDocument?> GetByIdAsync(int id);
+    Task<List<PdfDocument>> GetAllAsync();
+    Task<byte[]?> GetFileContentAsync(int id);
+    Task<int> SaveAsync(PdfDocument pdfDocument,byte[] content);
+    Task<bool> UpdateFormDatasAsync(int id, string formData);
+    Task<bool> DeleteAsync(int id);
+    Task<List<PdfDocument>> GetRecentDocumentsAsync(int count);
+}
