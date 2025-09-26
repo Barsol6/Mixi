5c0f7f42e607cb4e15c2f00e9cee0f067a903cc0
Barsol6 <bartosz.solyga@gmail.com>
2025-09-26 16:21:44 +0200

    feat: Inital prepartions for migration to MAUI. Added projects for Shared UI, Web API and for MAUI.

---
+++ Mixi.App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Mixi.App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
