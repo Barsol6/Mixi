4acc2028eca566b22e86021f5fc0e42f10399eca
Barsol6 <bartosz.solyga@gmail.com>
2025-09-27 17:25:27 +0200

    refactor: modified Signup.cs to match new architecture - removed direct access to UI.

--- Mixi.Api/Modules/Account/SignUp.cs
+++ Mixi.Api/Modules/Account/SignUp.cs
@@ -1,52 +1,43 @@
 ﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Mixi.Api.Modules.Database.Repositories.UserRepositories;
 using Mixi.Api.Modules.Users;
-using Mixi.Shared.Models.UI;
 
 namespace Mixi.Api.Modules.Account;
 public class SignUp
 {
     
-    public SignUp(SignUpPopup signUps, PasswordHash passwordHash, IUserRepository userRepository, ProtectedSessionStorage storage, Shared.Models.Account.Account account)
+    public SignUp(PasswordHash passwordHash, IUserRepository userRepository)
     {
-        SignUps = signUps;
         PasswordHash = passwordHash;
         UserRepository = userRepository;
-        Storage = storage;
-        Account = account;
     }
-
-    [Inject] private ProtectedSessionStorage Storage { get; set; }
+    
     [Inject] private IUserRepository UserRepository { get; set; }
     [Inject] private PasswordHash PasswordHash { get; set; }
-    [Inject] public SignUpPopup SignUps { get; set; }
-    
-    [Inject] public Shared.Models.Account.Account Account { get; set; }
     
     
     
     
-    public async Task<bool> CreateAccount()
+    
+    public async Task<bool> CreateAccount(string username, string password, string repeatpassword, string userType)
     {
-        Account.Password = PasswordHash.HashPasswords(Account.Password, Account.Username);
-        var user = new User { Username = Account.Username, Password = Account.Password, UserType = Account.UserType};
-        var exists = UserRepository.CheckIfExists(Account.Username);
-        Console.WriteLine(exists);
+        var hashedPassword = PasswordHash.HashPasswords(password, username);
+        
+        var exists = UserRepository.CheckIfExists(username);
         if (exists is true)
         {
             Console.WriteLine("Account exists");
-            Account.Username = String.Empty;
-            Account.Password = String.Empty;
-            Account.PasswordRepeat = String.Empty;
             return false;
         }
+        
+        var user = new User
+        {
+            Username = username, 
+            Password = password, 
+            UserType = userType
+        };
+     
         await UserRepository.AddUserAsync(user);
-        SignUps.IsVisible = false;
-        await Storage.SetAsync("SignUpPopupIsVisible", SignUps.IsVisible);
-        Account.Username = String.Empty;
-        Account.Password = String.Empty;
-        Account.PasswordRepeat = String.Empty;
         return true;
     }
     
