f24df80056a52ce0069f1c0f8c8a21dc42b83e8b
Barsol6 <bartosz.solyga@gmail.com>
2025-10-21 12:24:36 +0200

    feat: added notes menu

---
+++ Mixi.Shared.Models/UI/NotePopup.cs
@@ -0,0 +1,38 @@
+namespace Mixi.Shared.Models.UI;
+
+public class NotePopup
+{
+    private string _noteName;
+    
+    public event Action? NoteNameChanged;
+    
+    public string NoteName
+    {
+        get => _noteName;
+        set
+        {
+            if (_noteName != value)
+            {
+                _noteName = value;
+                NoteNameChanged?.Invoke();
+            }
+        }
+    }
+    
+    private bool _isVisible;
+    
+    public event Action? IsVisibleChange;
+    
+    public bool IsVisible
+    {
+        get => _isVisible;
+        set
+        {
+            if (_isVisible != value)
+            {
+                _isVisible = value;
+                IsVisibleChange?.Invoke();
+            }
+        }
+    }
+}
